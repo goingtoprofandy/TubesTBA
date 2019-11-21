59235128a4efaa7dd127e4d13d07f6c6315cb268
Perdana Faishal Mulhaq <meetfaishal@gmail.com>
2019-11-21 17:37:09 +0700

    versi ishal

--- main.js
+++ main.js
@@ -1,12 +1,49 @@
-var operand = [
-  ['p', 'q', 'r', 's'],
-  ['n', 'o', 't'],
-  ['a', 'n', 'd'],
-  ['o', 'r'],
-  ['x', 'o', 'r'],
-  ['i', 'f', 'f'],
-  ['i', 'f'],
-  ['t', 'h', 'e', 'n'],
-  ['('],
-  [')']
+const allOfOperand = [
+  'p',
+  'q',
+  'r',
+  's',
+  'not',
+  'and',
+  'or',
+  'xor',
+  'iff',
+  'if',
+  'then',
+  '(',
+  ')'
 ];
+
+const operand = [
+  ['p', 'q', 'r', 's'],
+  'not',
+  'and',
+  'or',
+  'xor',
+  'iff',
+  'if',
+  'then',
+  '(',
+  ')'
+];
+
+function removeSpace(data) {
+  const datas = [];
+  let newData = data.split(' ');
+
+  for (let index = 0; index < newData.length; index++) {
+    const element = newData[index];
+    if (element.length == 1) {
+      if (operand[0].includes(element)) datas.push(1);
+      else break;
+    } else if (operand.includes(element)) {
+      const tokenMin1 = operand.indexOf(element);
+      datas.push(tokenMin1 + 1);
+    } else break;
+  }
+  console.log(newData);
+  console.log(datas);
+}
+
+removeSpace('p or or p');
+// removeSpace('pq and rs');
